3e9d5bb7e6f0d90017f04577dc6530d176e9f995
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2015-04-07 10:16:50 -0600

    Added validates associated

--- lib/validation_scopes.rb
+++ lib/validation_scopes.rb
@@ -6,6 +6,19 @@ module ValidationScopes
     base.extend ClassMethods
   end
 
+  # Based on AssociatedValidator from ActiveRecord, see:
+  # activerecord-4.2.0/lib/active_record/validations/associated.rb @ line 3
+  class AssociatedValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      all_valid = Array.wrap(value).all? do |r|
+        r.marked_for_destruction? || r.send("no_#{options[:scope]}?")
+      end
+      unless all_valid
+        record.errors.add(attribute, :invalid, options.merge(:value => value))
+      end
+    end
+  end
+
   module ClassMethods
     def validation_scope(scope)
       @all_scopes ||= []
@@ -19,6 +32,12 @@ module ValidationScopes
 
       proxy_class = Class.new(DelegateClass(base_class)) do
         include ActiveModel::Validations
+
+        @scope = scope
+        def self.validates_associated(*attr_names)
+          validates_with AssociatedValidator,
+            _merge_attributes(attr_names).merge(scope: @scope)
+        end
 
         def initialize(record)
           @base_record = record
